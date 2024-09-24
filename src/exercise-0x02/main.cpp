0139215f50c7374e07e2fa7b0451705be68f627d
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-09-24 06:04:10 +0200

    Solution for exercise-0x02

--- src/exercise-0x02/main.cpp
+++ src/exercise-0x02/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "area.h"
 
 int main() {
   std::cout << "Exercise 0x02" << std::endl;
+  std::cout << area(2, 4) << std::endl;
+  std::cout << area(8) << std::endl;
+  std::cout << area(5, 6, 7) << std::endl;
+  std::cout << area(16, 32, 64) << std::endl;
   return 0;
 }
