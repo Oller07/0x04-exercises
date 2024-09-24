56abd7324016dfda350d12abe380a08d1e2e6e82
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-09-24 05:50:04 +0200

    Fix formatting

--- src/exercise-0x00/main.cpp
+++ src/exercise-0x00/main.cpp
@@ -3,15 +3,11 @@
 int find_max(int first, int second, int third);
 
 int main() {
-
   std::cout << "Exercise 0x00" << std::endl;  
-  
   int result = find_max(2, 4, 8);
   std::cout << result << std::endl;
-
   result = find_max(32, 16, 8);
   std::cout << result << std::endl;
-  
   result = find_max(64, 128, 32);
   std::cout << result << std::endl;
 
--- src/exercise-0x01/main.cpp
+++ src/exercise-0x01/main.cpp
@@ -4,21 +4,17 @@ int factorial(int n);
 
 int main() {
   std::cout << "Exercise 0x01" << std::endl;
-
   std::cout << factorial(-2) << std::endl;
   std::cout << factorial(0) << std::endl;
   std::cout << factorial(4) << std::endl;  
-
 
   return 0;
 }
 
 int factorial(int n) {
-  
   if(n < 0) {
     return -1;
   }
-  
   int result = 1;
   for(int i = n; i > 0; i--){
     result *= i;
